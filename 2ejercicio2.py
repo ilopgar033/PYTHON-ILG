65a39eaa9ecc589bcc8dd253401303a1ee6f35aa
ilopgar033 <ilopgar033@ieszaidinvergeles.org>
2025-10-07 13:36:53 +0200

    ejercicio2 de la segunda relacion

---
+++ 2ejercicio2.py
@@ -0,0 +1,6 @@
+# Pedir al usuario su nombre completo
+nombre_completo = input("Introduce tu nombre completo: ")
+
+print("Minúsculas:", nombre_completo.lower())
+print("Mayúsculas:", nombre_completo.upper())
+print("Primera letra:", nombre_completo.title())
