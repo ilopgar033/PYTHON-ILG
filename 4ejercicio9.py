aae2aae3e271c68d0ac79d798acb0bed207ecfaf
Iván López García <ilopgar033@ieszaidinvergeles.org>
2025-11-11 14:15:45 +0100

    9ejercicio de bucles

---
+++ 4ejercicio9.py
@@ -0,0 +1,5 @@
+contrasena_correcta = "contraseña"
+contrasena_usuario = ""
+while contrasena_usuario != contrasena_correcta:
+    contrasena_usuario = input("Introduce la contraseña: ")
+print("Contraseña correcta. ¡Acceso permitido!")
