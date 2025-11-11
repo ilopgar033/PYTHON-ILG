ef8ed2afbc5b1da67650c2191d0396f42d6b7b78
Iván López García <ilopgar033@ieszaidinvergeles.org>
2025-11-11 14:54:04 +0100

    12 ejercicio de bucles

---
+++ 4ejercicio12.py
@@ -0,0 +1,9 @@
+frase = input("Introduce una frase: ")
+letra = input("Introduce una letra para contar: ")
+
+contador = 0
+for caracter in frase:
+    if caracter == letra:
+        contador += 1 
+
+print(f"La letra '{letra}' aparece {contador} veces.")
